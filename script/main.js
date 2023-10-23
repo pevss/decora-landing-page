cb2a30d65570e0d299eda75183815b4aa86d66ed
pevss <pedrov3245@gmail.com>
2023-10-23 19:27:01 -0300

    beta

---
+++ script/main.js
@@ -0,0 +1,12 @@
+"use strict"
+
+document.getElementById("video-prototipo").style.display = "none"
+
+const input = document.querySelector("#valor")
+
+input.addEventListener("input", function(){
+    const valor = input.value
+    document.querySelector("#valor-interacao").textContent = `R$ ${valor * 10 + 990}`
+    //mudar a foto de fundo para o frame de acordo com o valor
+    // document.getElementById("interacao").style.backgroundImage = `url(../midias/frames/frame${valor}.jpg)`
+})
