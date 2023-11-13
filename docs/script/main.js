ae1fc4a65535d3f4c86c3b12d7157104a71cb6d4
pevss <pedrov3245@gmail.com>
2023-11-13 20:42:02 -0300

    correcao-versao-anterior

--- docs/script/main.js
+++ docs/script/main.js
@@ -10,17 +10,3 @@ input.addEventListener("input", function () {
     //mudar a foto de fundo para o frame de acordo com o valor
     // document.getElementById("interacao").style.backgroundImage = `url(../midias/frames/frame${valor}.jpg)`
 })
-
-function order(words) {
-    if (!words) return ""
-    const arr = words.split(" ")
-
-    let final = []
-    for (const i of arr.keys()) {
-        for (const part of arr) if (part.includes(`${i + 1}`)) final.push(part)
-    }
-
-    return final.join(" ")
-}
-
-console.log(order("is2 Thi1s T4est 3a"))
