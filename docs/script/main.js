8253ef986b15d3264ad2f342a8215170ad8a2a27
pevss <pedrov3245@gmail.com>
2023-11-13 19:19:59 -0300

    sem-imagens-e-videos

--- docs/script/main.js
+++ docs/script/main.js
@@ -4,9 +4,23 @@ document.getElementById("video-prototipo").style.display = "none"
 
 const input = document.querySelector("#valor")
 
-input.addEventListener("input", function(){
+input.addEventListener("input", function () {
     const valor = input.value
     document.querySelector("#valor-interacao").textContent = `R$ ${valor * 10 + 990}`
     //mudar a foto de fundo para o frame de acordo com o valor
     // document.getElementById("interacao").style.backgroundImage = `url(../midias/frames/frame${valor}.jpg)`
 })
+
+function order(words) {
+    if (!words) return ""
+    const arr = words.split(" ")
+
+    let final = []
+    for (const i of arr.keys()) {
+        for (const part of arr) if (part.includes(`${i + 1}`)) final.push(part)
+    }
+
+    return final.join(" ")
+}
+
+console.log(order("is2 Thi1s T4est 3a"))
